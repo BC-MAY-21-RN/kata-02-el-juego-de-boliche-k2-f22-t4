46e44846e52243ada598e3c1d852be3ae9b2e3e1
Kelly Alzate <kejoh07@gmail.com>
2022-02-16 23:17:25 +0100

    aliniacion del tablero primera prueba,
    Co-autored-by: Cris  <crprmahdz@yahoo.com>
    Co-authored-by: Kelly <kejoh07@gmail.com>
    Co-authored-by: Mario <tmario1508@gmail.com>
    Co-authored-by: Oscar <oscaravg27@gmail.com>
    Co-authored-by: Ronaldo <ronaldoadan1@gmail.com>
    Co-authored-by: Etneciv <flopez27@ucol.mx>
    Co-authored-by: Cris  <crprmahdz@yahoo.com>

--- boliche.js
+++ boliche.js
@@ -1,32 +1,48 @@
-/*class Board {
-    constructor(columns, rows) {
-        this.columns = columns;
-        this.rows = rows;
-    }
-    
-}*/
 
-var boardExample= [[4,5],[3,2],[10,0],[2,7],[3,5],[5,5],[5,4],[10,0],[1,9],[8,1]];
-
+var boardExample = [[1,4],[4,5],[6,4],[5,5],[10,0],[0,1],[7,3],[6,4],[10,0],[2,8]];
 
 class Turn {
     constructor(board){
         this.board = board;
     }
     
-    printBoard(board){
-        var boardPrint = this.board;
-        var points = [];
-        let firstT;
-        let secondT;
-        for(let i=0; i<board.length;i++){
-            firstT = boardPrint[i][0];
-            secondT = boardPrint[i][1];
-            points.push(firstT+secondT);
-            
-        }
-        return boardPrint;
+    printBoard(){
+        let firstT  = 0;
+        let secondT = 0;
+        let iTemp   = 0;
+        let iTotal  = 0;
+        let iToString = 0;
+        let sString1 = '';
+        let sTotales = '';
+        let sString2 = '';
+        this.board.forEach(function(value){
+            firstT = value[0];
+            secondT = value[1];
+            iTotal = firstT+secondT;
+            iTotal =(iTemp!=0 ? iTotal+iTemp : iTotal);
+            if(firstT+secondT==10 && firstT!=10){
+                secondT = '/'; // '/' = spare
+            }else if(firstT==10){
+                secondT = '*'; // '*' = pleno o chusa
+            }
+            if(secondT=='*'){ // Para alinear el primer renglón
+                sString1 += firstT+' '+secondT+'|';
+            }else{
+                sString1 += firstT+' '+secondT+' |';
+            }
+            iToString = iTotal;
+            iToString = iToString.toString().length;
+            if(iToString==2){ // Para alinear el segundo renglón
+                sString2 += iTotal+'  |';
+            }else{
+                sString2 += iTotal+'   |';
+            }
+            sTotales = sString1+'\n'+sString2;
+            iTemp = iTotal;
+            console.log(sTotales);
+        });
+        return sTotales;
     }
 }
-const a = new Turn();
-console.log(a.printBoard(boardExample));
+const a = new Turn(boardExample);
+console.log(a.printBoard());
